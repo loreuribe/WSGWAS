b0e77888c217e790ec5610217a8d7e5d68695fc7
Loena <Loena@BQINFORMATIK235>
2015-07-30 11:23:01 -0500

    Prueba de primer commit de la aplicación GWAS

---
+++ src/java/wsgwas/WSGWAS.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsgwas;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import snpBasicStatc.SNP;
+
+/**
+ *
+ * @author Loena
+ */
+@WebService(serviceName = "WSGWAS")
+public class WSGWAS {
+    SNP snpbasico;
+
+    /**
+     * This is a sample web service operation
+     */
+    
+    public WSGWAS()
+    {
+        snpbasico = new SNP();
+    }
+
+    @WebMethod(operationName = "ooddRatios")
+    public double oddRatios(@WebParam(name = "dato1") int dato1, @WebParam(name = "dato2") int dato2, @WebParam(name = "dato3") int dato3, @WebParam(name = "dato4") int dato4) {
+        return snpbasico.oddRatio(dato1,dato2,dato3,dato4);
+    }
+}
