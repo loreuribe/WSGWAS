b94b64deee297c80e403a6ffaa2134646fea554d
Loena <Loena@BQINFORMATIK235>
2015-07-30 11:34:33 -0500

    Cambio en el método procesamiento

--- src/java/wsgwas/WSGWAS.java
+++ src/java/wsgwas/WSGWAS.java
@@ -31,4 +31,14 @@ public class WSGWAS {
     public double oddRatios(@WebParam(name = "dato1") int dato1, @WebParam(name = "dato2") int dato2, @WebParam(name = "dato3") int dato3, @WebParam(name = "dato4") int dato4) {
         return snpbasico.oddRatio(dato1,dato2,dato3,dato4);
     }
+    
+    public double allelicFrecuenciasCountAllel_A(@WebParam(name = "genAA") int genAA, @WebParam(name = "GenAa") int GenAa) 
+    {
+        return 2*genAA+GenAa;
+    }
+    public double allelicFrecuenciasCountAllel_a(@WebParam(name = "genaa") int genaa, @WebParam(name = "GenAa") int GenAa) 
+    {
+        return 2*genaa+GenAa;
+    }
+    
 }
